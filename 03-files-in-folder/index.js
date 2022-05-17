8f8b6136efddd34ed0e6a9b2e967e34dd70c0493
Olga Antushevich <antushevich@gmail.com>
2022-05-17 17:11:12 +0300

    feat: add solution for the task 03 (Tue, May 17, 2022 05:11:12 PM)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const pathJoin = path.join(__dirname, './secret-folder');
+
+const fs = require('fs/promises');
+
+async function readDir() {
+  try {
+    const files = await fs.readdir(pathJoin, { withFileTypes: true });
+    for (const file of files) {
+      const filePath = path.join(__dirname, './secret-folder', file.name);
+      const fileNamePure = path.parse(filePath).name;
+      const stat = await fs.stat(filePath);
+
+      if (file.isFile()) {
+        process.stdout.write(`${fileNamePure} - ${path.extname(file.name).slice(1)} - ${stat.size} bytes\n`);
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+readDir();
