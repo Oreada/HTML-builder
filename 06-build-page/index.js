aba84a14be7e4630cc58118c6cd7aa830b812423
Olga Antushevich <antushevich@gmail.com>
2022-05-20 19:59:50 +0300

    feat: implement building of index.html for the task 06 (Fri, May 20, 2022 07:59:50 PM)

---
+++ 06-build-page/index.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const readline = require('readline');
+const stream = require('stream');
+
+const path = require('path');
+const pathJoinTemplate = path.join(__dirname, './template.html');
+const pathJoinComponents = path.join(__dirname, './components');
+const pathJoinProject = path.join(__dirname, './project-dist');
+const pathJoinIndexHtml = path.join(pathJoinProject, './index.html');
+
+async function noName() {
+  await fs.promises.mkdir(pathJoinProject, { recursive: true });
+  await fs.promises.writeFile(pathJoinIndexHtml, '', 'utf-8');
+
+  const inStream = fs.createReadStream(pathJoinTemplate);
+  const outStream = new stream;
+  const rl = readline.createInterface(inStream, outStream);  // построчное чтение './template.html'
+
+  //let templateValue = await fs.promises.readFile(pathJoinTemplate, 'utf-8');
+  // console.log(templateValue);
+
+  const files = await fs.promises.readdir(pathJoinComponents, { withFileTypes: true });
+
+  let lines = [];
+  rl.on('line', async function (line) {
+    lines.push(line);
+  });
+
+  rl.on('close', async function () {
+    for (let line of lines) {
+      let templateValue = line;
+      let indexFirst = line.indexOf('{{');
+      let indexLast = line.indexOf('}}');
+      if (indexFirst == -1 || indexLast == -1) {
+        await fs.promises.appendFile(pathJoinIndexHtml, templateValue + '\n', 'utf-8');
+      } else {
+        let nameFileComponent = line.slice(indexFirst + 2, indexLast);  // header||footer||articles||about...
+        for (const file of files) {
+          const filePath = path.join(__dirname, './components', file.name);
+          const fileNamePure = path.parse(filePath).name;  // name without extension
+          if (nameFileComponent === fileNamePure) {
+            let regExp = new RegExp(`{{${nameFileComponent}}}`, 'g');
+            let componentHtml = await fs.promises.readFile(filePath, 'utf-8');
+            templateValue = templateValue.replace(regExp, componentHtml);
+            await fs.promises.appendFile(pathJoinIndexHtml, templateValue + '\n', 'utf-8');
+            break;
+          }
+        }
+      }
+    }
+  });
+}
+
+noName();
